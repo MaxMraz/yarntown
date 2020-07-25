29d1b13e538382d8638de5d86248d2f59eefd793
maxmraz <max.mraz.11@gmail.com>
2020-07-24 20:45:03 -0400

    rifleman accidentally got a shotgun

--- data/enemies/huntsman_rifle.lua
+++ data/enemies/huntsman_rifle.lua
@@ -40,7 +40,7 @@ end)
 function enemy:choose_attack()
 	if enemy:get_distance(hero) <= GUN_RANGE then
 		require("enemies/lib/attacks/gun"):attack(enemy, {
-      num_bullets = 5,
+      num_bullets = 1,
       max_bullet_distance = 120,
     })
 	else
